8774777166ce2607cb6bd371fb882b78e1fd5b84
Your Name <you@example.com>
2023-03-17 00:05:04 -0600

    links

--- src/components/Hero/Banners.js
+++ src/components/Hero/Banners.js
@@ -12,10 +12,10 @@ const Banners = () => {
   return (
     <div className={style.container}>
         <div className={style.content}>
-            <Banner className={'a'} source={wipers} link={'bosch'} text={'ESCOBILLAS DE'} highlight={'ALTA CALIDAD'}/>
+            <Banner className={'a'} source={wipers} link={'/bosch'} text={'ESCOBILLAS DE'} highlight={'ALTA CALIDAD'}/>
             <Banner className={'b'} source={rims} link={'/searchresult/ACCESORIOS'} text={'LOS MEJORES'} highlight={'ACCESORIOS'} />
-            <Banner className={'c'} source={oil} link={'pennzoil'} text={'MEJOR'} highlight={'LUBRICACIÓN'} />
-            <Banner className={'d'} source={coolant} link={'coolant'} text={'ENFRÍA'} highlight={'TU MOTOR'} />
+            <Banner className={'c'} source={oil} link={'/pennzoil'} text={'MEJOR'} highlight={'LUBRICACIÓN'} />
+            <Banner className={'d'} source={coolant} link={'/coolant'} text={'ENFRÍA'} highlight={'TU MOTOR'} />
         </div>
     </div>
   )
